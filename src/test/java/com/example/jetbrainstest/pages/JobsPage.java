76798b8c7187e22015393f589dfb824bf0ddc54c
neckapb <golodyuk@tut.by>
2025-07-26 23:03:52 +0300

    added test

---
+++ src/test/java/com/example/jetbrainstest/pages/JobsPage.java
@@ -0,0 +1,37 @@
+package com.example.jetbrainstest.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+
+public class JobsPage {
+
+    WebDriver driver;
+
+    @FindBy(css = "._content_ud38m4_207")
+    private WebElement findYourDreamJob;
+
+    @FindBy(css = "._button_937yxw_133")
+    private WebElement jobRoles;
+
+    public Boolean checkDream() {
+        System.out.println("Проверка активности кнопки *Find*Your*Dream*Job*");
+        return findYourDreamJob.isEnabled();
+    }
+
+    public void checkDreamInList() {
+        findYourDreamJob.click();
+        // ожидание должно быть здесь?
+        jobRoles.click();
+        //ожидание? ловим список из ._content_4bkrr3_40
+        List<WebElement> roles = driver.findElements(By.cssSelector("._content_4bkrr3_40"));
+    }
+
+    public JobsPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+}
